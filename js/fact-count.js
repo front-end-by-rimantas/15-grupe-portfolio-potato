e7280048f26f63fc273a5d2e0885bcfb4d66ee9c
Albrit197 <63236021+Albrit197@users.noreply.github.com>
2020-05-28 02:01:34 +0300

    font update

--- js/fact-count.js
+++ js/fact-count.js
@@ -4,24 +4,23 @@ function renderFactCount( selector, data ) {
     // validacija
     const DOM = document.querySelector(selector);
     
-    if( !DOM ) {
-        throw 'error: could'
+    if ( !DOM ) {
+        throw 'error: could not find element to render new content'
     }
     
 
     // logika
-    console.log(selector);
-    console.log(data);
-
+    
     let HTML = '';
     for ( let i=0; i<data.length; i++) {
+        const skill = data[i];
         HTML += `
-          <div class="col-4 col-lg-4 col-sm-12">
+          <div class="col-4 col-md-12 count" data-animated="false">
             <div class="experience">
              <div class="round">
-                <span class="value">8</span>
+                <span class="value">${skill.value}</span>
              </div>
-             experience
+             <div class="exp">${skill.title}</div>
            </div>
          </div>`;     
     }
@@ -30,6 +29,7 @@ function renderFactCount( selector, data ) {
     
 
     // rezultato grazinimas
+    DOM.innerHTML = HTML;
 
     return;
     
--- js/fact-home.js
+++ js/fact-home.js
@@ -4,8 +4,25 @@ import skills from './fact-data.js';
 import renderFactCount from './fact-count.js';
 
 
-console.log('home page...');
-// console.log(skills);
+const countSelector = '#skills .row';
 
-renderFactCount('#skills .row', skills );
+renderFactCount(countSelector, skills );
 
+const factCounts = document.querySelectorAll( countSelector + '> .count');
+
+
+
+window.addEventListener('scroll', () => {
+    const windowBottomHeight = window.scrollY + window.innerHeight;
+    
+    for ( let i=0; i<factCounts.length; i++) {
+    const c = factCounts[i];
+    const cBottomHeight = c.offsetTop + c.offsetHeight;
+
+   if (cBottomHeight < windowBottomHeight) {
+       c.dataset.animated = 'true';
+       
+   } 
+
+   }
+});
